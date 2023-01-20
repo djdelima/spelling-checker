fac52d463ce34ffacc5203c4eeeb3c7e45dd7301
diego.delima <diego.delima@dazn.com>
2023-01-20 17:42:20 +0100

    feat: add basic frontend and cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   // const document = SwaggerModule.createDocument(app, options);
   // SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
