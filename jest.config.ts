0ea76c5a4e9572379f84458d98a842c9f61462dd
diego.delima <diego.delima@dazn.com>
2023-01-24 18:45:01 +0100

    feat: add jest configs and test coverage

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import baseConfig from './jest.base.config';
+
+export default {
+  ...baseConfig,
+  testRegex: '.spec.ts$',
+  coverageDirectory: '<rootDir>/../coverage',
+  cacheDirectory: '<rootDir>/../.cache/jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/**/*.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!<rootDir>/**/**/*.integration.test.ts',
+  ],
+  coverageReporters: [['lcov', { projectRoot: './' }], 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 82,
+      functions: 62,
+      lines: 75,
+      statements: 74,
+    },
+  },
+};
