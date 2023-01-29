b1bf538cf4a66cae8c70ef3123f0bb644a695169
diego.delima <diego.delima@dazn.com>
2023-01-29 18:20:32 +0100

    test: fic script and action

--- .github/workflows/openapi-code-suggestions.sh
+++ .github/workflows/openapi-code-suggestions.sh
@@ -42,10 +42,10 @@ do
         # Add suggestions as a comment on the pull request
         curl \
           -X POST \
-          -H "Accept: application/vnd.github+json" \
-          -H "Authorization: Bearer $GITHUB_TOKEN"\
-          -H "X-GitHub-Api-Version: 2022-11-28" \
-          https://api.github.com/repos/$OWNER/$REPO/issues/$PULL_REQUEST_NUMBER/comments \
-          -d '{\"body\":\"$(cat suggestions.txt)\"}'
+          -H "Accept: application/vnd.github3+json" \
+          -H "Authorization: token $GITHUB_TOKEN"\
+          -H "Content-Type: application/json" \
+          -d '{\"body\":\"$(cat suggestions.txt)\"}' \
+          https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_REQUEST_NUMBER/reviews
     fi
 done
