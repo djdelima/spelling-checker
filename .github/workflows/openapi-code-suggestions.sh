c9f18a4b690608b1ceddd5e06a744ebcbe298f5f
diego.delima <diego.delima@dazn.com>
2023-01-29 19:02:14 +0100

    test: fic script and action

--- .github/workflows/openapi-code-suggestions.sh
+++ .github/workflows/openapi-code-suggestions.sh
@@ -20,13 +20,13 @@ do
     if [[ $FILE =~ $FILE_EXTENSIONS ]]
     then
         # Get contents of file
-        echo "https://api.github.com/repos/$OWNER/$REPO/contents/$FILE?ref=pull/$PULL_REQUEST_NUMBER/head";
-        CONTENTS=$(curl -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/contents/$FILE?ref=pull/$PULL_REQUEST_NUMBER/head")
+#        echo "https://api.github.com/repos/$OWNER/$REPO/contents/$FILE?ref=pull/$PULL_REQUEST_NUMBER/head";
+#        CONTENTS=$(curl -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/contents/$FILE?ref=pull/$PULL_REQUEST_NUMBER/head")
 
-        DECODED_CONTENT="$PROMPT $(echo $CONTENTS | jq -r '.content' | base64 --decode)"
+#        DECODED_CONTENT="$PROMPT $(echo $CONTENTS | jq -r '.content' | base64 --decode)"
 
         # escape the special characters
-        PROMPT=$(printf "%q" "$DECODED_CONTENT")
+#        PROMPT=$(printf "%q" "$DECODED_CONTENT")
 
        payload='{
            "prompt": "import { createCircuitBreaker } from './circuit-breaker'; import { LoggerService } from '../logger.service'; import CircuitBreaker, { Options } from 'opossum'; import { GrammarBotError } from '../errors/grammar-bot.error'; describe('createCircuitBreaker', () => { let logger: LoggerService; let func: jest.Mock; let breaker: CircuitBreaker; beforeEach(() => { logger = new LoggerService(); func = jest.fn(); breaker = createCircuitBreaker(func, logger); }); it('should log \"TIMEOUT: is taking too long to respond.\" when the timeout event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('timeout'); expect(logger.log).toHaveBeenCalledWith( 'TIMEOUT: is taking too long to respond.', ); }); it('should log \"OPEN: The breaker just opened.\" when the open event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('open'); expect(logger.log).toHaveBeenCalledWith('OPEN: The breaker just opened.'); }); it('should log \"HALF_OPEN: The breaker is half open.\" when the halfOpen event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('halfOpen'); expect(logger.log).toHaveBeenCalledWith( 'HALF_OPEN: The breaker is half open.', ); }); it('should log \"CLOSE: The breaker has closed. Service OK.\" when the close event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('close'); expect(logger.log).toHaveBeenCalledWith( 'CLOSE: The breaker has closed. Service OK.', ); }); it('should log \"Circuit breaker fallback: error message\" and throw a GrammarBotError when the fallback event is emitted with code EOPENBREAKER', () => { jest.spyOn(logger, 'error'); const error = { code: 'EOPENBREAKER', message: 'error message' }; expect(() => breaker.emit('fallback', error)).toThrow(GrammarBotError); expect(logger.error).toHaveBeenCalledWith( 'Circuit breaker fallback: error message', ); }); });",
@@ -37,15 +37,15 @@ do
 
         # Use OpenAI API to generate code suggestions
         curl https://api.openai.com/v1/completions -H "Content-Type: application/json" -H "Authorization: Bearer $API_KEY" -d "{\"prompt\":\"$escaped_payload\",\"model\":\"babbage\"}" > suggestions.txt
-        echo "Suggestions: $(cat suggestions.txt)"
 
+        echo "STARTING A PUTARIA!!!!"
         # Add suggestions as a comment on the pull request
         curl \
           -X POST \
           -H "Accept: application/vnd.github3+json" \
           -H "Authorization: token $GITHUB_TOKEN"\
           -H "Content-Type: application/json" \
-          -d '{\"body\":\"$(cat suggestions.txt)\"}' \
+          -d "{\"body\":\"$(cat suggestions.txt | jq -r '.text')"} \
           https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_REQUEST_NUMBER/reviews
     fi
 done
