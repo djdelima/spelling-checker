288ebf1f345265e1f679ca003bb405f7ca6b6ef6
diego.delima <diego.delima@dazn.com>
2023-01-29 17:55:41 +0100

    test: fic script and action

--- .github/workflows/openapi-code-suggestions.sh
+++ .github/workflows/openapi-code-suggestions.sh
@@ -30,15 +30,13 @@ do
 
        payload='{
            "prompt": "import { createCircuitBreaker } from './circuit-breaker'; import { LoggerService } from '../logger.service'; import CircuitBreaker, { Options } from 'opossum'; import { GrammarBotError } from '../errors/grammar-bot.error'; describe('createCircuitBreaker', () => { let logger: LoggerService; let func: jest.Mock; let breaker: CircuitBreaker; beforeEach(() => { logger = new LoggerService(); func = jest.fn(); breaker = createCircuitBreaker(func, logger); }); it('should log \"TIMEOUT: is taking too long to respond.\" when the timeout event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('timeout'); expect(logger.log).toHaveBeenCalledWith( 'TIMEOUT: is taking too long to respond.', ); }); it('should log \"OPEN: The breaker just opened.\" when the open event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('open'); expect(logger.log).toHaveBeenCalledWith('OPEN: The breaker just opened.'); }); it('should log \"HALF_OPEN: The breaker is half open.\" when the halfOpen event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('halfOpen'); expect(logger.log).toHaveBeenCalledWith( 'HALF_OPEN: The breaker is half open.', ); }); it('should log \"CLOSE: The breaker has closed. Service OK.\" when the close event is emitted', () => { jest.spyOn(logger, 'log'); breaker.emit('close'); expect(logger.log).toHaveBeenCalledWith( 'CLOSE: The breaker has closed. Service OK.', ); }); it('should log \"Circuit breaker fallback: error message\" and throw a GrammarBotError when the fallback event is emitted with code EOPENBREAKER', () => { jest.spyOn(logger, 'error'); const error = { code: 'EOPENBREAKER', message: 'error message' }; expect(() => breaker.emit('fallback', error)).toThrow(GrammarBotError); expect(logger.error).toHaveBeenCalledWith( 'Circuit breaker fallback: error message', ); }); });",
-           "model": "code-davinci-002",
-           "language": "javascript"
        }'
 
        # Escape the special characters in the payload using jq
        escaped_payload=$(echo "$payload" | jq -c .)
 
         # Use OpenAI API to generate code suggestions
-        curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $API_KEY" -d payload https://api.openai.com/v1/completions > suggestions.txt
+        curl https://api.openai.com/v1/completions -H "Content-Type: application/json" -H "Authorization: Bearer $API_KEY" -d "{\"prompt\":\"$escaped_payload\",\"model\":\"code-davinci-002\",\"language\":\"javascript\"}" > suggestions.txt
         echo "Suggestions: $(cat suggestions.txt)"
 
         # Add suggestions as a comment on the pull request
